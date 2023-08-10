8ec244c712204f3279729390ad9d879446190563
vanphu1997pro <anhtenphu1997@gmail.com>
2023-08-10 11:33:53 +0700

    them bien d

--- tinhtong/Program.cs
+++ tinhtong/Program.cs
@@ -12,8 +12,10 @@ namespace tinhtong
         {
             int a = 3;
             int b = 5;
-            int c = a + b;
-            Console.WriteLine("tong 2 so {0} va {1} la: {2}",a,b,c);
+            // thêm biến d có giá trị là 6 
+            int d = 6;
+            int c = a + b +d;
+            Console.WriteLine("tong 3 so {0} va {1} va {2} la: {3}",a,b,d, c);
             Console.ReadLine();
 
         }
