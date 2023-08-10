5c44a21dd8b516edf180fb895eada13cec9ffa0b
vanphu1997pro <anhtenphu1997@gmail.com>
2023-08-10 11:18:00 +0700

    init project

---
+++ tinhtong/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tinhtong
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3;
+            int b = 5;
+            int c = a + b;
+            Console.WriteLine("tong 2 so {0} va {1} la: {2}",a,b,c);
+            Console.ReadLine();
+
+        }
+    }
+}
