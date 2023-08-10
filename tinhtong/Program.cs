c2f797262dbb797994fa69b1172bfcf3163a8ece
vanphu1997pro <anhtenphu1997@gmail.com>
2023-08-10 11:49:38 +0700

    add sum + 360

--- tinhtong/Program.cs
+++ tinhtong/Program.cs
@@ -12,10 +12,14 @@ namespace tinhtong
         {
             int a = 3;
             int b = 5;
-            // thêm biến d có giá trị là 6 
+            // thêm biến d có giá trị là 6 "phú thêm"
             int d = 6;
             int c = a + b +d;
             Console.WriteLine("tong 3 so {0} va {1} va {2} la: {3}",a,b,d, c);
+           
+           //tính tổng với 360  "phú thêm"
+            int s = c + 360;
+            Console.WriteLine("tổng của {0} khi cộng với 360 = {1}",c,s);
             Console.ReadLine();
 
         }
